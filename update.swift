e92b3ac63840b2199124a91ef5c3b4e3471ad3ac
Felix Herrmann <felix.herrmann@icloud.com>
2021-12-14 23:34:50 +0100

    reverted to Combine

--- update.swift
+++ update.swift
@@ -1,4 +1,5 @@
 import Foundation
+import Combine
 
 
 // MARK: - Models
@@ -15,57 +16,50 @@ struct ReleasePayload: Encodable {
 }
 
 
-// MARK: - Errors
+// MARK: - Publishers
 
-enum UpdateError: Error {
-    case fetchReleases(statusCode: Int)
-    case createRelease(statusCode: Int)
-    case packageDotSwift
-    case noAccessToken
-    case updatePackageDotSwift(Error)
-    case pushChanges(Error)
+func releasesPublisher(url: URL) -> AnyPublisher<[Release], Never> {
+    URLSession.shared.dataTaskPublisher(for: url)
+        .map(\.data)
+        .decode(type: [Release].self, decoder: decoder)
+        .replaceError(with: [])
+        .eraseToAnyPublisher()
 }
 
-
-// MARK: - Networking
-
-func releases(from url: URL) async throws -> [Release] {
-    let (data, response) = try await URLSession.shared.data(from: url)
-    if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode >= 400 {
-        throw UpdateError.fetchReleases(statusCode: httpResponse.statusCode)
-    } else {
-        let decoder = JSONDecoder()
-        decoder.keyDecodingStrategy = .convertFromSnakeCase
-        let releases = try decoder.decode([Release].self, from: data)
-        return releases
+func createReleasePublisher(accessToken: String, url: URL, payload: ReleasePayload) -> Future<Void, Error> {
+    return Future { promise in
+        let encoder = JSONEncoder()
+        encoder.keyEncodingStrategy = .convertToSnakeCase
+        let data: Data
+        do {
+            data = try encoder.encode(payload)
+        } catch {
+            promise(.failure(error))
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.setValue("application/vnd.github.v3+json", forHTTPHeaderField: "Accept")
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue("token \(accessToken)", forHTTPHeaderField: "Authorization")
+        request.httpMethod = "POST"
+        request.httpBody = data
+        
+        URLSession.shared.dataTask(with: request) { _, _, error in
+            if let error = error {
+                promise(.failure(error))
+            } else {
+                promise(.success(()))
+            }
+        }.resume()
     }
 }
 
-func createRelease(accessToken: String, url: URL, payload: ReleasePayload) async throws {
-    let encoder = JSONEncoder()
-    encoder.keyEncodingStrategy = .convertToSnakeCase
-    let data = try encoder.encode(payload)
-    
-    var request = URLRequest(url: url)
-    request.setValue("application/vnd.github.v3+json", forHTTPHeaderField: "Accept")
-    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-    request.setValue("token \(accessToken)", forHTTPHeaderField: "Authorization")
-    request.httpMethod = "POST"
-    request.httpBody = data
-    
-    let (_, response) = try await URLSession.shared.data(for: request)
-    if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode >= 400 {
-        throw UpdateError.createRelease(statusCode: httpResponse.statusCode)
-    }
-}
-
-func packageDotSwift(from url: URL) async throws -> String {
-    let (data, _) = try await URLSession.shared.data(from: url)
-    if let packageDotSwift = String(data: data, encoding: .utf8) {
-        return packageDotSwift
-    } else {
-        throw UpdateError.packageDotSwift
-    }
+func packageDotSwiftPublisher(url: URL) -> AnyPublisher<String, Never> {
+    URLSession.shared.dataTaskPublisher(for: url)
+        .compactMap { String(data: $0.data, encoding: .utf8) }
+        .replaceError(with: "")
+        .eraseToAnyPublisher()
 }
 
 
@@ -81,75 +75,88 @@ func git(_ args: String...) throws {
 }
 
 
-// MARK: - Helpers
+// MARK: - Execution
 
-func accessToken() throws -> String {
-    guard CommandLine.arguments.count > 1 else { throw UpdateError.noAccessToken }
-    return CommandLine.arguments[1]
+guard let intercomURL = URL(string: "https://api.github.com/repos/intercom/intercom-ios/releases"),
+      let packageURL = URL(string: "https://api.github.com/repos/FelixHerrmann/intercom-ios/releases"),
+      let packageDotSwiftURL = URL(string: "https://raw.githubusercontent.com/intercom/intercom-ios/master/Package.swift") else {
+    exit(EXIT_FAILURE)
 }
 
-func updatePackageDotSwift(with newContent: String) throws {
+guard CommandLine.arguments.count > 1 else {
+    print("No access token specified")
+    exit(EXIT_FAILURE)
+}
+let accessToken = CommandLine.arguments[1]
+
+let decoder = JSONDecoder()
+decoder.keyDecodingStrategy = .convertFromSnakeCase
+
+var cancellables: Set<AnyCancellable> = []
+
+let a = releasesPublisher(url: intercomURL)
+let b = releasesPublisher(url: packageURL)
+
+Publishers.Zip(a, b)
+    .sink { zip in
+        guard let intercomLatest = zip.0.first, let packageLatest = zip.1.first else {
+            print("One of the repositories has no releases")
+            exit(EXIT_FAILURE)
+        }
+        if intercomLatest.tagName == packageLatest.tagName {
+            print("Version \(packageLatest.tagName) is latest")
+            exit(EXIT_SUCCESS)
+        } else {
+            packageDotSwiftPublisher(url: packageDotSwiftURL)
+                .sink { packageDotSwift in
+                    guard packageDotSwift != "" else {
+                        print("New Package.swift not available")
+                        exit(EXIT_FAILURE)
+                    }
+                    updatePackageDotSwift(with: packageDotSwift)
+                    pushChanges(release: intercomLatest)
+                    
+                    let payload = ReleasePayload(tagName: intercomLatest.tagName, name: intercomLatest.tagName, body: intercomLatest.body)
+                    createReleasePublisher(accessToken: accessToken, url: packageURL, payload: payload)
+                        .sink { completion in
+                            switch completion {
+                            case .failure(let error):
+                                print("Create release failed: ", error)
+                                exit(EXIT_FAILURE)
+                            case .finished:
+                                print("Release \(intercomLatest.tagName) created successfully")
+                                exit(EXIT_SUCCESS)
+                            }
+                        } receiveValue: { _ in }
+                        .store(in: &cancellables)
+                }
+                .store(in: &cancellables)
+        }
+    }
+    .store(in: &cancellables)
+
+RunLoop.current.run()
+
+
+func updatePackageDotSwift(with newContent: String) {
     let file = FileManager.default.currentDirectoryPath + "/Package.swift"
     do {
         try newContent.write(toFile: file, atomically: true, encoding: .utf8)
+        print("Package.swift updated successfully")
     } catch {
-        throw UpdateError.updatePackageDotSwift(error)
+        print("Update Package.swift file failed: ", error)
+        exit(EXIT_FAILURE)
     }
 }
 
-func pushChanges(release: Release) throws {
+func pushChanges(release: Release) {
     do {
         try git("add", ".")
         try git("commit", "-m", "Bump to \(release.tagName)")
         try git("push")
-    } catch {
-        throw UpdateError.pushChanges(error)
-    }
-}
-
-
-// MARK: - Execution
-
-@main
-struct Update {
-    
-    static func main() async throws {
-        guard let intercomReleasesURL = URL(string: "https://api.github.com/repos/intercom/intercom-ios/releases"),
-              let packageReleasesURL = URL(string: "https://api.github.com/repos/FelixHerrmann/intercom-ios/releases"),
-              let packageDotSwiftURL = URL(string: "https://raw.githubusercontent.com/intercom/intercom-ios/master/Package.swift") else {
-            exit(EXIT_FAILURE)
-        }
-        
-        let accessToken = try accessToken()
-        
-        print("Checking and comparing releases ...")
-        let intercomReleases = try await releases(from: intercomReleasesURL)
-        let packageReleases = try await releases(from: packageReleasesURL)
-        guard let intercomLatest = intercomReleases.first, let packageLatest = packageReleases.first else {
-            print("One of the repositories has no releases")
-            exit(EXIT_FAILURE)
-        }
-        guard intercomLatest.tagName != packageLatest.tagName else {
-            print("Version \(packageLatest.tagName) is latest")
-            exit(EXIT_SUCCESS)
-        }
-        
-        print("Updating Package.swift ...")
-        let packageDotSwift = try await packageDotSwift(from: packageDotSwiftURL)
-        guard packageDotSwift != "" else {
-            print("New Package.swift not available")
-            exit(EXIT_FAILURE)
-        }
-        try updatePackageDotSwift(with: packageDotSwift)
-        print("Package.swift updated successfully")
-        
-        print("Pushing changes to master ...")
-        try pushChanges(release: intercomLatest)
         print("Changes pushed to master successfully")
-        
-        print("Creating release \(intercomLatest.tagName) ...")
-        let payload = ReleasePayload(tagName: intercomLatest.tagName, name: intercomLatest.tagName, body: intercomLatest.body)
-        try await createRelease(accessToken: accessToken, url: packageReleasesURL, payload: payload)
-        print("Release \(intercomLatest.tagName) created successfully")
+    } catch {
+        print("Push git failed: ", error)
+        exit(EXIT_FAILURE)
     }
 }
